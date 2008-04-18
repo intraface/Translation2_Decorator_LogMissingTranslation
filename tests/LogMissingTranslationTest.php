e22e092b841a4ebf429f43d6ec8cf2cee4056261
Lars Olesen <lars@intraface.dk>
2008-04-18 11:13:09 +0000

    Added build file.
    git-svn-id: http://svn.intraface.dk/intrafacepublic/3Party/Translation2_Decorator_LogMissingTranslation@886 2fc64ab5-e821-0410-98e1-ee8f871d7b73

---
+++ tests/LogMissingTranslationTest.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+
+class LogMissingTranslationTest extends PHPUnit_Framework_TestCase
+{
+    function testTest()
+    {
+        $this->assertTrue(true);
+    }
+}
